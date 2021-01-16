b9d1534a27d43c67711ba7a12cb81b943b744420
GhaidEwawi <ghaid_99@hotmail.com>
2021-01-16 12:14:27 +0200

    complete face detection step

---
+++ face_detection.py
@@ -0,0 +1,39 @@
+!pip install face_recognition
+
+import face_recognition
+import PIL.Image
+import PIL.ImageDraw
+
+# Storing the image in a variable
+image = face_recognition.load_image_file('people.jpg')
+
+# Finding face locations in the image (pretrained HOG model)
+face_locations = face_recognition.face_locations(image)
+
+number_of_faces = len(face_locations)
+print(f"I found {number_of_faces} face(s) in this photograph")
+
+# Load the image into a python Image Library object so that we can draw on top of it and display it
+# this line only copies the images into the structure of pil_image.. because pill uses its own format.
+# We did this because we'll use pil library for drawing a box around the detected face.
+pil_image = PIL.Image.fromarray(image)
+
+for face_location in face_locations:
+
+    # Print the location of each face in this image, Each face is a list of co-ordinates in (top, right, bottom, left) order.
+    top, right, bottom, left = face_location
+    print(f"A face is located at pixel location Top: {top}, Left: {left}, Bottom: {bottom}, Right: {right}")
+
+    # Let's draw a box around the face
+    draw = PIL.ImageDraw.Draw(pil_image)
+    draw.rectangle([left, top, right, bottom], outline="red")
+
+# Display the image on screen
+pil_image.show() # this didn't work so I used the code block below
+
+# Commented out IPython magic to ensure Python compatibility.
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+plt.imshow(pil_image)
+plt.show()
